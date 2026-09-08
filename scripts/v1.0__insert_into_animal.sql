487848ed5299c4a76582a2b4a410fdf952f12f82
Geovana Jochem Santos <geovanajochemsantos@gmail.com>
2026-09-08 16:22:59 -0400

    Create v1.0__insert_into_animal.sql

---
+++ scripts/v1.0__insert_into_animal.sql
@@ -0,0 +1,4 @@
+INSERT INTO animal (id_cliente,nome,especie,raca,sexo,data_nascimento)
+VALUES ('2','Shakira','Gato','Rajado','F','2024/08/20'),
+  	('2','Frederico','Gato','Rajado','F','2021/12/07'),
+  	('1','Maria Antonieta','Gato','Rajado','F','2024/12/25');
