d1dea8b7ade9f0e64156e577d09beec4b2e35ffd
Geovana Jochem Santos <geovanajochemsantos@gmail.com>
2026-09-08 16:07:38 -0400

    Create v1.0__insert_into_cliente.sql

---
+++ scripts/v1.0__insert_into_cliente.sql
@@ -0,0 +1,3 @@
+INSERT INTO cliente (nome,cpf,telefone,endereco)
+VALUES ('Geovana Jochem Santos','123.456.789-00','69 912345678','rua 1 casa 90'),
+    	('Murilo Manoel de Almeida','538.815.242-15','69 984460725','rua 2 casa 50');
