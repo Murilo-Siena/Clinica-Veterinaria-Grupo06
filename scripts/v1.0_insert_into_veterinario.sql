4ac52ded59b91dbf841bf493f817cef2237660ab
beca-gs <garcia.rebeca.0708@gmail.com>
2026-09-08 15:53:39 -0400

    Insert multiple veterinarians into the database

---
+++ scripts/v1.0_insert_into_veterinario.sql
@@ -0,0 +1,7 @@
+SELECT * FROM veterinario;
+
+INSERT INTO veterinario (nome, cpf, crmv, especialidade, telefone)
+VALUES ('Jeronimo Dias', '143.231.897-65', '1564', 'Cardiologista', '69987432582'),
+	     ('Jussara Lemos', '050.784.219-43', '3861', 'Cirurgião', '69873205676'),
+	     ('Leonardo da Vinci', '398.463.610-76', '5890', 'Demartologista', '69450987321'),
+	     ('Sandra Silva', '320.589.102-80', '0918', 'Oncologista', '69432908764');
