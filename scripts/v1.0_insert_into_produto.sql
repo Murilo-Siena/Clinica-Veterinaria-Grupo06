28c48d6cda75a0e6eba427d82752911633c882fa
Geovana Jochem Santos <geovanajochemsantos@gmail.com>
2026-09-08 15:58:14 -0400

    Create v1.0_insert_into_produto.sql

---
+++ scripts/v1.0_insert_into_produto.sql
@@ -0,0 +1,3 @@
+INSERT INTO produto (nome,tipo,marca,descricao,valor_compra,estoque)
+VALUES ('Bravecto cães','Medicação','Bravecto','compimidos mastigáveis contra carrapatos e pulgas','169.90','10'),
+        ('Promun dog','Suplemento','Organnact','Suplemento para cães idosos','158.58','31');
