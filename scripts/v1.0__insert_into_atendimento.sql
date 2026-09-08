f979bfd8b0304d4af1948e778046d81ae7bf148e
beca-gs <garcia.rebeca.0708@gmail.com>
2026-09-08 16:48:20 -0400

    Insert multiple records into atendimento table

---
+++ scripts/v1.0__insert_into_atendimento.sql
@@ -0,0 +1,7 @@
+SELECT * FROM atendimento;
+
+INSERT INTO atendimento (data, descricao_consulta, id_atendente, id_cliente, id_animal, id_veterinario)
+VALUES 
+('2026/03/20', 'Frederico realizou um hemograma completo para verificar sua saúde', '1', '2', '4', '3'),
+('2026/08/08', 'Shakira tomou as suas vacinas pendentes', '2', '1', '2', '4'),
+('2026/09/09', 'Maria Antonieta fez uma consulta de rotina', '1', '2', '3', '1');
