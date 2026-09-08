4b60771bde0345cab55ac921c3a1cc6a1ad0955e
Murilo Manoel de Almeida <murilomanoelpvh@gmail.com>
2026-09-08 15:29:13 -0400

    Create produto table with necessary fields

---
+++ scripts/v1.0_create_table_produto.sql
@@ -0,0 +1,12 @@
+CREATE TABLE produto (
+    id_produto BIGINT GENERATED ALWAYS AS IDENTITY,
+    nome VARCHAR(100),
+    tipo VARCHAR(50),
+    marca VARCHAR(100),
+    descricao TEXT,
+    valor_compra NUMERIC(10,2),
+    estoque INTEGER,
+
+    CONSTRAINT pk_produto
+        PRIMARY KEY (id_produto)
+);
